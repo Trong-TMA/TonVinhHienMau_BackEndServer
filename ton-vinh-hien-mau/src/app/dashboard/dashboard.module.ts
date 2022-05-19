00d8738331897bb2ddc337413384ed70997f6217
Trong-TMA <81163984+Trong-TMA@users.noreply.github.com>
2022-05-19 09:40:04 +0700

    fronend

---
+++ ton-vinh-hien-mau/src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,19 @@
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard.component';
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+  ],
+  exports:[
+    DashboardComponent,
+  ]
+})
+export class DashboardModule { }
